c468b5cb54cc8cd1ec48a416b71cccd02edb7cc4
Oleksandr Bekas <lxbekas@gmail.com>
2023-04-10 22:31:34 +0300

    Complete.

---
+++ js-hw-04/js/03.js
@@ -0,0 +1,27 @@
+// # Завдання 3 (2 бали)
+// 
+// Напиши функцію `findLongestWord(string)`, яка приймає параметром довільний рядок
+// (в рядку будуть тільки слова і прогалини) і повертає найдовше слово в цьому
+// рядку.
+
+ 
+const findLongestWord = function (string) {
+    let words = string.split(' ');
+    var longestWord = "";
+
+    for(var i = 0; i < words.length; i++){
+        if(words[i].length > longestWord.length){
+            longestWord = words[i];
+        }
+    }
+    return longestWord;
+};
+
+/*
+ * Виклич функції для перевірки працездатності твоєї реалізації.
+ */
+console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'
+
+console.log(findLongestWord('Google do a roll')); // 'Google'
+
+console.log(findLongestWord('May the force be with you')); // 'force'
